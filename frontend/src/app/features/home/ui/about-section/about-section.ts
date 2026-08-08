8fd45f6d91218617375ee54e84f2df712130fdd8
florianLopitaux <florian.lopitaux@gmail.com>
2026-08-08 17:59:56 +0200

    Implemented about section component for the home page

--- frontend/src/app/features/home/ui/about-section/about-section.ts
+++ frontend/src/app/features/home/ui/about-section/about-section.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-about-section',
+  selector: 'section[app-about-section]',
   imports: [],
   templateUrl: './about-section.html',
   styleUrl: './about-section.scss',
